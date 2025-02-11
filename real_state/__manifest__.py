d49ce412d1155cff1f03b5b4c5a34e645a6b8f62
Ebramsobhy <abramsobhy7@gmail.com>
2025-02-11 11:35:34 +0200

    Creating Tree View For Property Model

--- real_state/__manifest__.py
+++ real_state/__manifest__.py
@@ -2,6 +2,9 @@
     'name' : 'RealState',
     'author' : 'Ebram Sobhy',
     'depends' : ['base'],
-    'data' : [],
+    'data' : [
+          'views/base_menu.xml', 
+          'views/property_view.xml'
+    ],
     'application': True,
 }
